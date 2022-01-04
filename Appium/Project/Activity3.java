ac018a851ef38876e60b9721fde68543d0415184
poulami92 <94598228+poulami92@users.noreply.github.com>
2022-01-04 09:22:17 +0530

    Update Activity3.java

--- Appium/Project/Activity3.java
+++ Appium/Project/Activity3.java
@@ -1,91 +1,89 @@
-package AppiumProjectActivity;
+package com.google.AppiumActivities;
 
-import org.testng.annotations.Test;
-
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.AutomationName;
-import io.appium.java_client.remote.MobileCapabilityType;
-
-import org.testng.annotations.BeforeClass;
+import static org.testng.Assert.assertFalse;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-public class Activity3 {
-AndroidDriver<MobileElement> driver= null;
-WebDriverWait wait;
-	
-	@BeforeClass
-	 public void setUp() throws MalformedURLException {
-		
-		DesiredCapabilities caps= new DesiredCapabilities();
-		caps.setCapability("deviceName", "Pixel4Emulator");
-		caps.setCapability("platformName", "android");
-		caps.setCapability("automationName", "UiAutomator2");
-		caps.setCapability("appPackage", "com.android.chrome");
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+
+public class Activity_3_ToDoList {
+    AppiumDriver<MobileElement> driver = null;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "<device name>");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appPackage", "com.android.chrome");
         caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
-		caps.setCapability("noReset", true);
-				
-		URL appServer = new URL("http://127.0.0.1:4723/wd/hub");
-		driver = new AndroidDriver<MobileElement>(appServer,caps);
-		wait = new WebDriverWait(driver, 40);
-		
-	}
-	
-	
-  @Test
-  public void addNote() {
-	  
-	  driver.get("https://www.training-support.net/selenium");
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.xpath("//android.widget.Button[@text='Get Started!']")));
-	  driver.findElement(MobileBy.AndroidUIAutomator("UiScrollable(new UiSelector().scrollable(true)).scrollForward(8).scrollIntoView(textContains(\"To-Do\"))"));
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.xpath("//android.view.View[contains(@text,'Elements get added')]"))).click();
-	  
-		/*
-		 * MobileElement inputTask = (MobileElement)
-		 * wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.xpath(
-		 * "//android.widget.EditText")));
-		 * 
-		 * MobileElement saveTask = (MobileElement)
-		 * wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.
-		 * xpath("//android.widget.Button[@text='Add Task']")));
-		 */
-		/*
-		 * List<String> tasks = new ArrayList<String>();
-		 * 
-		 * tasks.add("Add tasks to list"); tasks.add("Get number of tasks");
-		 * tasks.add("Clear the list");
-		 * 
-		 * for (String task : tasks) {
-		 * 
-		 * inputTask.sendKeys(task); saveTask.click(); }
-		 */
-	  
-	  
-	  
-	 
-	  
-	  
-	  
-	  
-  }
-  
+        caps.setCapability("noReset", true);
 
-  @AfterClass
-  public void tearDown() {
-	  
-	 //driver.quit();
-  }
+        // Instantiate Appium Driver
+        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
+        driver = new AndroidDriver<MobileElement>(appServer, caps);
+        wait = new WebDriverWait(driver, 15);
 
+        driver.get("https://www.training-support.net/selenium");
+    }
+
+    @Test
+    public void addToDoList() throws Exception {
+    	String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
+    	wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.className("android.view.View")));
+    	
+    	driver.findElement(
+                MobileBy.AndroidUIAutomator(UiScrollable + ".scrollForward(7).scrollIntoView(new UiSelector().textContains(\"To-Do List\"))"))
+                .click();
+    wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.className("android.widget.EditText")));	
+   //Task 1
+    driver.findElementByClassName("android.widget.EditText").sendKeys("Add tasks to list");
+    driver.findElementByXPath("//*[contains(@text, 'Add Task')]").click();
+    driver.findElementByXPath("//*[contains(@text, 'Add tasks to list')]").click();
+    Thread.sleep(5000);
+    //Task 2
+    driver.findElementByClassName("android.widget.EditText").sendKeys("Clear the list");
+    driver.findElementByXPath("//*[contains(@text, 'Add Task')]").click();
+    driver.findElementByXPath("//*[contains(@text, 'Clear the list')]").click();
+    Thread.sleep(5000);
+    //Task 3
+    driver.findElementByClassName("android.widget.EditText").sendKeys("Get number of tasks");
+    driver.findElementByXPath("//*[contains(@text, 'Add Task')]").click();
+    driver.findElementByXPath("//*[contains(@text, 'Get number of tasks')]").click();
+    Thread.sleep(5000);
+    //clear list
+    
+    driver.findElementByXPath("//*[contains(@text, 'Clear List')]").click();
+    Thread.sleep(5000);
+    //
+    
+    MobileElement message = driver.findElementByXPath("//*[contains(@text, 'Add more tasks to this list.')]");
+    Assert.assertFalse(message.isDisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //driver.quit();
+    }
 }
